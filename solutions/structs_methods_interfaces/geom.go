65970ef6e81ec35ebd59f7a7e8b926024baaba53
tomdoesdev <94772873+tomdoesdev@users.noreply.github.com>
2025-07-10 18:39:02 +1200

    finish exercise

--- solutions/structs_methods_interfaces/geom.go
+++ solutions/structs_methods_interfaces/geom.go
@@ -1,5 +1,37 @@
 package structs_methods_interfaces
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+type Rectangle struct {
+	Width  float64
+	Length float64
+}
+
+func (r *Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Length)
+}
+
+func (r *Rectangle) Area() float64 {
+	return r.Width * r.Length
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t *Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
--- solutions/structs_methods_interfaces/geom_test.go
+++ solutions/structs_methods_interfaces/geom_test.go
@@ -1,12 +1,36 @@
 package structs_methods_interfaces
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rect := Rectangle{10.0, 10.0}
+
+	got := rect.Perimeter()
 	want := 40.0
 
 	if got != want {
 		t.Errorf("got %.2f, want %.2f", got, want)
 	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{&Rectangle{12, 6}, 72.0},
+		{&Circle{10}, math.Pi * math.Pow(10, 2)},
+		{&Triangle{12, 6}, 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g, want %g", tt.shape, got, tt.want)
+		}
+	}
+
 }
